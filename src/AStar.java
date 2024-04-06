20d99b936bb1b9721b7b9d2622fe4fe58b8f8024
S3rum <tasospa2001@gmail.com>
2024-04-06 04:19:34 +0300

    v0.8.2

--- src/AStar.java
+++ src/AStar.java
@@ -9,6 +9,13 @@ public class AStar {
         startNode = node;
         run();
     }
+    public Node getMinHNode(Node node){
+        int min = node.gethList().get(0);
+        int position = 0;
+        for(int i = 1; i < node.gethList().size(); i++){
+
+        }
+    }
     public void run(){
         Node currNode = startNode;
         boolean loop = true;
@@ -18,14 +25,24 @@ public class AStar {
             int min = currNode.gethList().get(0);
             int position = 0;
             for(int i = 1; i < currNode.gethList().size(); i++){
-                if(currNode.gethList().get(i) < min){
-                    min = currNode.gethList().get(i);
-                    position = i;
-                } else if (currNode.gethList().get(i) == min) {
-                    min = currNode.gethList().get(i);
+
+                if(!currNode.getParent().isDupe(currNode.getChildren().get(i))){
+                    if(currNode.gethList().get(i) < min){
+                        min = currNode.gethList().get(i);
+                        position = i;
+                    } else if (currNode.gethList().get(i) == min) {
+                        min = currNode.gethList().get(i);
+                    }
                 }
+
             }
-            if (currNode.isDupe(currNode.getChildren().get(position))){
+            if(currNode.getParent() == null){
+                currNode = currNode.getChildren().get(position);
+                Board board = new Board(currNode.getState());
+                System.out.println("CHOSEN BOARD");
+                board.display();
+                System.out.println("BASTARD=========================================");
+            } else if (currNode.getParent().isDupe(currNode.getChildren().get(position))){
                 currNode = currNode.getChildren().get(position + 1);
                 Board board = new Board(currNode.getState());
                 System.out.println("CHOSEN BOARD");
@@ -37,7 +54,6 @@ public class AStar {
                 System.out.println("CHOSEN BOARD");
                 board.display();
                 System.out.println("=========================================");
-
             }
             if(min == 0){
                 loop = false;
