058c4353f0e7c2086dd8251f43ab35b555e194d6
S3rum <tasospa2001@gmail.com>
2024-04-09 09:34:03 +0300

    v0.8.2

--- src/AStar.java
+++ src/AStar.java
@@ -1,17 +1,15 @@
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class AStar {
     Node startNode;
+    private List<Node> visitedList = new ArrayList<Node>();
     public AStar(Node node){
         startNode = node;
         run();
     }
     public Node getMinHNode(Node node){
         int min = node.gethList().get(0);
-        int position = 1;
+        int position = 0;
         for(int i = 0; i < node.gethList().size(); i++){
             if(node.getParent() == null){
                 if(node.gethList().get(i) < min) {
@@ -19,22 +17,35 @@ public class AStar {
                     position = i;
                 }
             }
-            else if(!node.getParent().isDupe(node.getChildren().get(i))){
-                if(node.gethList().get(i) < min) {
-                    min = node.gethList().get(i);
-                    position = i;
+            else if(!node.getParent().isDupe(node.getChildren().get(i))){ //Check if parent is same as a children
+                if(deepContains(node)){
+                    if(node.gethList().get(i) < min) {  //Check if it is smaller than then current min
+                        min = node.gethList().get(i);
+                        position = i;
+                    } else if (node.gethList().get(i) == min){ //Check if it is equal to the current min
+                        position = i;
+                    }
                 }
             }
         }
         return node.getChildren().get(position);
+    }
+
+    public boolean deepContains(Node node) {
+        for (Node element : visitedList) {
+            if (Arrays.deepEquals(element.getState(), node.getState()))
+                return true;
+        }
+        return false;
     }
     public void run(){
         Node currNode = startNode;
         boolean loop = true;
         while (loop){
             PossibleStates possibleStates = new PossibleStates(currNode);
-            createHList(currNode);
-            currNode = getMinHNode(currNode);
+            createHList(currNode); //create the h for each child
+            currNode = getMinHNode(currNode); //move the current node based on the h values
+            visitedList.add(currNode);
             Board board = new Board(currNode.getState());
             System.out.println("CHOSEN BOARD");
             board.display();
